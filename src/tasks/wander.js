7f6bcca29e1fbd02fcdcf799b1dcf00d898717ac
wangchengkai <me@chengkai.wang>
2018-06-05 16:14:47 +0800

    update

--- src/tasks/wander.js
+++ src/tasks/wander.js
@@ -1,16 +1,18 @@
 const puppeteer = require('puppeteer');
 const static = require('../utils/static');
+const _ = require('lodash');
 
 const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
 
 const grabbed = {}; // 已处理url
-const queued = {}; // url队列
+let queued = []; // url队列
 
 /**
  *
  * @param {string} url 入口url
  */
 async function wander(url = 'http://wangchengkai.com', browser) {
+  console.log('----start to process ' + url);
   try {
     const p = await browser.newPage();
     await p.close();
@@ -24,18 +26,43 @@ async function wander(url = 'http://wangchengkai.com', browser) {
     deviceScaleFactor: 2
   });
   await page.setUserAgent(UA);
+  let _hrefs = [];
+  try {
   await page.goto(url, {waitUntil: 'networkidle2'});
   // const content = await page.content();
-  const content = '';
-  await page.$eval('html', node => {
-    content = node.innerHTML;
-  }, content);
-  console.log(content);
-  return;
-  // const content = html.innerHTML;
-  const currentUrl = await page.url();
-  await static(currentUrl, content);
-  grabbed[currentUrl] = 1;
+  const context = await page.evaluate((() => {
+    const links = [...document.getElementsByTagName('a')];
+    links.forEach(a => {
+      a.setAttribute('href', a.href);
+    });
+    const hrefs = links.map(link => link.href);
+    let _url = location.href;
+    _url = _url.replace(location.hash, '');
+    _url = _url.replace(location.search, '');
+    return {
+      html: document.documentElement.outerHTML,
+      url: _url,
+      fullUrl: location.href,
+      hrefs,
+      pathname: location.pathname
+    };
+  }));
+  const { content, url:clearUrl, fullUrl, pathname, hrefs } = context;
+  _hrefs = hrefs;
+  await static(clearUrl, pathname, content);
+  } catch(e) {
+    console.log('err:', e);
+  }
+  grabbed[url] = 1;
+  queued2 = _.filter(_.uniq([..._hrefs, ...queued]), link => !grabbed[link]);
+  console.log(_.difference(queued2, queued), 'added to queue!');
+  queued = queued2;
+  if (queued.length) {
+    wander(queued.shift());
+  } else {
+    await browser.close();
+    console.log('finished');
+  }
 }
 
 
--- src/utils/static.js
+++ src/utils/static.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-function static(url, content) {
-  const match = url.match(/\/\/([^\/])+\/([^#?]*)/);
-  const _path = match[2] || 'index';
-  const dir = path.resolve(__dirname, '../../static_pages', `${_path}.html`);
+
+function static(url, pathname, content) {
+  const relativePath = pathname === '/' ? 'index.html' : pathname.replace(/(\/)?$/, '') + '.html';
+  const dist = path.resolve(__dirname, '../../static_pages', relativePath);
+  const documentType = `<!DOCTYPE html>
+  `;
+  console.log(dist);
   return new Promise((resolve, reject) => {
-    fs.writeFile(dir, content, 'utf-8', function (err, data) {
+    fs.writeFile(dist, documentType + content, 'utf-8', function (err, data) {
       if (err) return reject(err);
       resolve(data);
     });
